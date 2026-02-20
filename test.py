0d3b15bc1650a2ee0b3b8665731461210b25b09c
Khurram Javed <mail@khurramjaved.com>
2026-02-20 11:38:30 -0500

    Update the benchmark and add stochastic policies

--- test.py
+++ test.py
@@ -1,22 +1,32 @@
-import gym
-game = "Pong" +"NoFrameskip-v4"
-env = gym.make(game)
-# This seed is fixed and was used
-# by the pre-trained agents to record actions.
-# Changing it will break the benchmark
-env.seed(1)
+import base64, json, pickle, sys
+from ale_py import ALEInterface, roms
 
+trace = json.load(open(sys.argv[1]))
+rom = trace["env"].split("-v")[0].replace("NoFrameskip", "").replace("Deterministic", "")
 
-with open("policies/"+ game +".txt", "rb") as f:
-    byte = f.read(1)
-    ep_reward = 0
-    while byte != b"":
-        val = int.from_bytes(byte, 'big')
-        if(val==82):
-            print("Resetting; prev epiode return ", ep_reward)
-            env.reset()
-            ep_reward = 0
-        else:
-            obs, r, _, _ = env.step(val - 97)
-            ep_reward += r
-        byte = f.read(1)
+ale = ALEInterface()
+ale.setInt("random_seed", int(trace["test_seed"]))
+ale.setFloat("repeat_action_probability", 0.0)
+ale.setInt("frame_skip", 1)
+ale.loadROM(getattr(roms, rom))
+ale.reset_game()
+ale.restoreState(pickle.loads(base64.b64decode(trace["ale_state"])))
+
+actions = list(ale.getMinimalActionSet())
+ep_return = 0.0
+episode_returns = []
+
+for c in trace["sequence_of_actions"]:
+    if c == "R":
+        episode_returns.append(ep_return)
+        print(episode_returns)
+        ep_return = 0.0
+        ale.reset_game()
+    else:
+        ep_return += ale.act(actions[ord(c) - ord("a")])
+
+if ep_return != 0:
+    episode_returns.append(ep_return)
+
+print("episode_returns =", episode_returns)
+print("expected_returns =", trace.get("episode_returns"))
